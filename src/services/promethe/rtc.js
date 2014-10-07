2015d4c1bb80f5b56e40d1bff974a183773770fb
Sylvain Mahé <sylv.mahe@gmail.com>
2014-10-07 18:40:59 +0200

    add licence headers

--- src/services/promethe/rtc.js
+++ src/services/promethe/rtc.js
@@ -1,3 +1,19 @@
+/* maya-client
+ *
+ * Copyright (c) 2014, Partnering Robotics, All rights reserved.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version
+ *  3.0 of the License This library is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
+
+
 var util = require('util');
 
 
