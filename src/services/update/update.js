c1193bde0601a386eb07217768113dd9f19400fe
Leila Meziou <leila.meziou@gmail.com>
2015-04-30 11:30:38 +0200

    new service for update packages

---
+++ src/services/update/update.js
@@ -0,0 +1,81 @@
+/* maya-client
+ *
+ * Copyright (c) 2014, Partnering Robotics, All rights reserved.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version
+ *  3.0 of the License. This library is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
+var util = require('util');
+
+
+var Message = require('../message');
+
+function Update(node){	
+	var that = this;
+	this.node = node;
+	return this;
+}
+
+Update.prototype.listPackages = function(callback){
+
+	this.node.get({
+		service: 'update',
+		func: 'ListPackages'
+	}, function(data){
+		if(data.packages) 
+			callback(null,data.packages); 
+		if(data.error)
+			callback(data.error,null);
+		
+	}); 
+}
+	
+Update.prototype.updateAll = function(callback){
+
+	this.node.get({
+		service: 'update',
+		func: 'UpdateAll'
+	}, function(data){
+		if(data.packages) 
+			callback(null,data.packages); 
+		if(data.error)
+			callback("Error in updateAll: "+data.error,null);
+	}); 
+}
+	
+Update.prototype.installPackage = function(pkg, callback){
+		
+	if ((pkg === 'Undefined') || (typeof pkg !== 'string') || (pkg.length < 2)){
+		callback('undefinedPackage',null);
+	}
+			
+	this.node.get({
+		service: 'update',
+		func: 'InstallPackage',
+		data:{
+			package: pkg,
+		}
+	}, function(data){
+		if(data.packages) 
+			callback(null,data.packages); 
+		if(data.error)
+			callback(data.error,null);
+			
+		
+		
+	});
+}
+		
+		
+var exp = {
+		Update: Update
+}
+
+module.exports = exp; 
