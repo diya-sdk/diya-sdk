998e772b09170aa16c43e8c69ef437151805ee9e
Sylvain Mahe <sylv.mahe@gmail.com>
2015-05-24 15:46:45 +0200

    added d1 selector

--- src/diya-sdk.js
+++ src/diya-sdk.js
@@ -1,276 +1,168 @@
 var Q = require('q');
 
-//////////////////////////////////////////////////////////////
-/////////////////// Logging utility methods //////////////////
-//////////////////////////////////////////////////////////////
+var DiyaNode = require('./DiyaNode');
 
-var DEBUG = true;
-var Logger = {
-	log: function(message){
-		if(DEBUG) console.log(message);
-	},
+var connections = [];
+var tokens = [];
 
-	error: function(message){
-		if(DEBUG) console.error(message);
-	}
+function d1(selector){
+	return new DiyaSelector(selector);
 }
 
-//////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////
+d1.DiyaNode = DiyaNode;
 
+d1.connect = function(addr){
 
-function DiyaNode(addr){
-	this._addr = addr;
-	this._socket = null;
-
-	this._nextId = 0;
-
-	this._pendingMessages = [];
-}
-
-////////////////////////////////////////////////////
-////////////////// Public API //////////////////////
-////////////////////////////////////////////////////
-
-DiyaNode.prototype.connect = function(WSocket){
-	var that = this;
-	var deferred = Q.defer();
-
-	if(!WSocket) WSocket = window.WebSocket;
-	this._socket = new WSocket(this._addr);
-
-	this._socket.onopen = function(){
-		deferred.resolve();
-		that._socket.onclose = that._onclose.bind(that);
-		that._socket.onmessage = that._onmessage.bind(that);
-	}
-	this._socket.onerror = function(err){
-		if(deferred.promise.isFulfilled()){
-			Logger.error("[WS] error : "+err);
-		}else {
-			deferred.reject(err);
-		}
+	//If connection already exists, return the connection promise associated with it
+	for(var i=0;i<connections.length; i++){
+		if(connections[i].handler._addr === addr)
+		 	return connections[i].promise;
 	}
 
-	return deferred.promise;
-};
+	//create and init the connection to remote DiyaNode
+	var handler = new DiyaNode(addr);
+	var promise = handler.connect();
 
+	//Add connection to the connections array
+	connections.push({
+		handler: handler,
+		promise: promise
+	});
 
-DiyaNode.prototype.request = function(params, callback, timeout){
-	var that = this;
-
-	var message = this._createMessage(params, "Request");
-	message.id = this._generateId();
-	this._appendMessage(message, callback);
-
-	if(!isNaN(timeout) && timeout > 0){
-		setTimeout(function(){
-			var handler = that._removeMessage(message.id);
-			if(handler) that._notifyListener(handler, 'Timeout exceeded ('+timeout+'ms) !');
-		}, timeout);
-	}
-
-	if(!this._send(message)){
-		this._removeMessage(message.id);
-		Logger.error('Cannot send request !');
-		return false;
-	}
-
-	return true;
-};
-
-DiyaNode.prototype.subscribe = function(params, callback){
-	var message = this._createMessage(params, "Subscription");
-	message.id = this._generateId();
-	this._appendMessage(message, callback);
-
-	if(!this._send(message)){
-		this._removeMessage(message.id);
-		Logger.error('Cannot send subscription !');
-		return -1;
-	}
-
-	return message.id;
-};
-
-DiyaNode.prototype.unsubscribe = function(subid){
-	if(this._pendingMessages[subid] && this._pendingMessages[subid].type === "Subscription"){
-		this._removeMessage(subid);
-	}
-};
-
-///////////////////////////////////////////////////////////
-//////////////////// Internal methods /////////////////////
-///////////////////////////////////////////////////////////
-
-DiyaNode.prototype._appendMessage = function(message, callback){
-	this._pendingMessages[message.id] = {
-		callback: callback,
-		type: message.type,
-		target: message.target
-	};
-};
-
-DiyaNode.prototype._removeMessage = function(messageId){
-	var handler = this._pendingMessages[messageId];
-	if(handler){
-		delete this._pendingMessages[messageId];
-		return handler;
-	}else{
-		return null;
-	}
-};
-
-DiyaNode.prototype._clearMessages = function(err, data){
-	for(var messageId in this._pendingMessages){
-		var handler = this._removeMessage(messageId);
-		this._notifyListener(handler, err, data);
-	}
-};
-
-DiyaNode.prototype._getMessageHandler = function(messageId){
-	var handler = this._pendingMessages[messageId];
-	return handler ? handler : null;
-};
-
-DiyaNode.prototype._notifyListener = function(handler, error, data){
-	if(handler && typeof handler.callback === 'function') {
-		error = error ? error : null;
-		data = data ? data : null;
-		handler.callback(error, data);
-	}
-};
-
-DiyaNode.prototype._send = function(message){
-	try{
-		var data = JSON.stringify(message);
-	}catch(err){
-		Logger.error('Cannot serialize message');
-		return false;
-	}
-
-	try{
-		this._socket.send(data);
-	}catch(err){
-		Logger.error('Cannot send message');
-		return false;
-	}
-
-	return true;
-}
-
-///////////////////////////////////////////////////////////////
-/////////////////// Socket event handlers /////////////////////
-///////////////////////////////////////////////////////////////
-
-DiyaNode.prototype._onmessage = function(evt){
-	try{
-		var message = JSON.parse(evt.data);
-	}catch(err){
-		Logger.error("[WS] cannot parse message, dropping...");
-		return ;
-	}
-
-	if(isNaN(message.id)) {
-		this._handleInternalMessage(message);
-	}else{
-		var handler = this._getMessageHandler(message.id);
-		if(handler){
-			switch(handler.type){
-				case "Request":
-					this._handleRequest(handler, message);
-					break;
-				case "Subscription":
-					this._handleSubscription(handler, message);
-					break;
+	//Remove the connection from connections array when connection is closed
+	function removeConnection(){
+		for(var i=0; i<connections.length; i++){
+			if(connections[i].handler === handler){
+				connections.splice(i, 1);
+				break;
 			}
 		}
-	}
+	};
+
+	handler.onclose = removeConnection;
+	promise.catch(removeConnection);
+
+	//return a promise that will be
+	return promise;
 };
 
-DiyaNode.prototype._onclose = function(){
-	Logger.log("[WS] connection closed !");
-	this._clearMessages();
-	if(typeof this.onclose === 'function') this.onclose();
+
+function DiyaSelector(selector){
+	this.length = 0;
+	if(selector.constructor.name === 'String') this._selectString(selector);
+	else if(selector.constructor.name === 'RegExp') this._selectRegExp(selector);
+}
+
+DiyaSelector.prototype._selectString = function(selector){
+	var that = this;
+
+	this._select(function(peer){
+		//If selector match peer name, select peer and
+		//stop exploring peers by returning false
+		if(peer.id === selector){
+			that.length = 1;
+			that[0] = peer;
+			return false;
+		}
+		//Otherwise keep exploring peers
+		return true;
+	})
 };
 
-/////////////////////////////////////////////////////////////
-/////////////// Protocol event handlers /////////////////////
-/////////////////////////////////////////////////////////////
+DiyaSelector.prototype._selectRegExp = function(selector){
+	var that = this;
 
-DiyaNode.prototype._handleInternalMessage = function(message){
-	switch(message.type){
-		case "PeerConnected":
-			this._handlePeerConnected(message);
-			break;
-		case "PeerDisconnected":
-			this._handlePeerDisconnected(message);
-			break;
-	}
+	this._select(function(peer){
+		if(peer.id.match(selector)){
+			that[that.length] = peer;
+			that.length += 1;
+		}
+		return true;
+	});
 };
 
-DiyaNode.prototype._handlePeerConnected = function(message){
-	//TODO
-};
-
-DiyaNode.prototype._handlePeerDisconnected = function(message){
-	if(message.peerId === undefined){
-		Logger.error("Missing arguments for PeerDisconnected Message, dropping...");
-		return ;
-	}
-
-	//Go through all pending messages and notify the ones that are targeted
-	//at the disconnected peer that it disconnected and therefore the command
-	//cannot be fulfilled
-	for(var messageId in this._pendingMessages){
-		var handler = this._getMessageHandler(messageId);
-		if(handler && handler.target === message.peerId) {
-			this._removeMessage(messageId);
-			this._notifyListener(handler, null, null);
+DiyaSelector.prototype._select = function(selectorFunction){
+	for(var i=0;i<connections.length; i++){
+		var peers = connections[i].handler.peers();
+		for(var j=0; j<peers.length; j++){
+			if(!selectorFunction({id: peers[j], handler: connections[i].handler})) break;
 		}
 	}
 };
 
-DiyaNode.prototype._handleRequest = function(handler, message){
-	this._removeMessage(message.id);
-	this._notifyListener(handler, message.error, message.data);
+DiyaSelector.prototype.each = function(cb){
+	for(var i=0; i<this.length; i++) cb(this[i]);
+	return this;
 };
 
-DiyaNode.prototype._handleSubscription = function(handler, message){
-	//remove subscription if it was closed from node
-	if(message.result === "closed") this._removeMessage(message.id);
-	this._notifyListener(handler, message.error, message.data ? message.data : null);
+DiyaSelector.prototype.request = function(params, callback, timeout){
+	return this.each(function(peer){
+		params.target = peer.id;
+		params.token = tokens[peer.id];
+		peer.handler.request(params, function(err, data){
+			callback(peer.id, err, data);
+		}, timeout);
+	});
 };
 
-///////////////////////////////////////////////////////////////
-////////////////////// Utility methods ////////////////////////
-///////////////////////////////////////////////////////////////
-
-DiyaNode.prototype._createMessage = function(params, type){
-	if(!params || !type || !params.service || (type !== "Request" && type !== "Subscription")){
-		return null;
-	}
-
-	return {
-		type: type,
-		service: params.service,
-		target: params.target,
-		token: params.token,
-		func: params.func,
-		obj: params.obj,
-		data: params.data
-	}
+DiyaSelector.prototype.subscribe = function(params, callback){
+	var subIds = [];
+	this.each(function(peer){
+		params.target = peer.id;
+		params.token = tokens[peer.id];
+		var subId = peer.handler.subscribe(params, function(err, data){
+			callback(peer.id, err, data);
+		});
+		subIds.push({subId: subId, peerId: peer.id});
+	});
+	return subIds;
 };
 
-DiyaNode.prototype._generateId = function(){
-	var id = this._nextId;
-	this._nextId++;
-	return id;
+DiyaSelector.prototype.auth = function(user, password, callback, timeout){
+	return this.request({
+		service: 'auth',
+		func: 'Authenticate',
+		data: {
+			user: user,
+			password: password
+		}
+	}, function(peerId, err, data){
+		if(!err && data.authenticated && data.token){
+			tokens[peerId] = data.token;
+			callback(peerId, true);
+		}else {
+			callback(peerId, false);
+		}
+	}, timeout);
 };
 
 
+module.exports = d1;
 
-module.exports = {
-	DiyaNode: DiyaNode
-}
+
+///////////////////////////////////////////////
+/*
+d1.connect("ws://partneringportal.fr/api");
+d1.connect("ws://10.42.0.1:1234");
+
+d1("RobotOne").auth("admin", "pass");
+
+d1("RobotOne").request({
+	service: "timer",
+	func: "SubscribeTimer"
+}, function(err, data){
+
+});
+
+d1("cofely.*").install("libmayartc");
+d1("cofely.*").update("diya-node");
+d1("cofely.*").updateAll("diya-node");
+
+var rtc = d1("cofely.d1p1").rtc()
+	.use("ctrl.info", onInfoNeuron)
+	.use("ctrl.force", onForceNeuron)
+	.onclose(onclose)
+
+rtc.connect();
+rtc.close();
+*/
