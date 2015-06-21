c3959226adf88e810de8ebdb5b00f9f4622419ea
Quang VO <quangvnm@hotmail.com>
2015-06-21 11:24:20 +0200

    add service map

--- src/diya-sdk.js
+++ src/diya-sdk.js
@@ -7,5 +7,6 @@ require('./services/pico/pico.js');
 require('./services/viewer_explorer/viewer_explorer.js');
 require('./services/ieq/ieq.js');
 require('./services/networkId/NetworkId.js');
+require('./services/maps/maps.js');
 
 module.exports = d1;
