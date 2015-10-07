4093274e7cc730ee6ac57eb45beba4d178b3f003
jfellus <jerome.fellus@partnering.fr>
2015-10-07 09:14:07 +0200

    #37-SecuredAuthentication SecuredAuthentication

--- src/services/peerAuth/PeerAuth.js
+++ src/services/peerAuth/PeerAuth.js
@@ -1,24 +1,24 @@
 var DiyaSelector = require('../../DiyaSelector').DiyaSelector;
 
 
-
 /**
  * Make the selected DiyaNodes join an existing DiyaNodes Mesh Network by contacting
  * the given bootstrap peers.
- * 
- * NOTE : This operation requires root role 
- * 
+ *
+ * NOTE : This operation requires root role
+ *
  * @param bootstrap_peers
  */
 DiyaSelector.prototype.join = function(bootstrap_peers, callback){
-	if(typeof bootstrap_peers === 'string') bootstrap_peers = [ boostrap_peers ];
-	if(typeof bootstrap_peers !== 'array') throw "join() : bootstrap_peers should be an array of peers URIs";
+	if(typeof bootstrap_peers === 'string') bootstrap_peers = [ bootstrap_peers ];
+	if(typeof bootstrap_peers.constructor === Array) throw "join() : bootstrap_peers should be an array of peers URIs";
 
 	return this.givePublicKey(function(joining_peer, err, data){
-		
-		Logger.error("YOUPI !! " + data.keys().join(","));
-		
-		d1(bootstrap_peers).addTrustedPeer();
+		if(err) ERR("The PeerAuth service is not running on " + joining_peer);
+		else {
+			ERR("YOUPI !! ");
+			d1(bootstrap_peers).addTrustedPeer();
+		}
 	});
 };
 
@@ -28,7 +28,7 @@ DiyaSelector.prototype.join = function(bootstrap_peers, callback){
  */
 DiyaSelector.prototype.givePublicKey = function(callback){
 	return this.request(
-		{ service: 'peerAuth',	func: 'GivePublicKey',	data: {} }, 
+		{ service: 'peerAuth',	func: 'GivePublicKey',	data: {} },
 		function(peerId, err, data){callback(peerId,err,data);
 	});
 };
