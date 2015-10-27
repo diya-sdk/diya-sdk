74309bf3b122355b7fcc833ec458c8e323f44fbd
Jerome Fellus <jfellus@gmail.com>
2015-10-27 23:34:04 +0100

    ...

--- src/DiyaNode.js
+++ src/DiyaNode.js
@@ -54,7 +54,7 @@ DiyaNode.prototype.connect = function(addr, WSocket){
 	}
 
 	if(this._addr === addr){
-		console.log("SMAE " + this._status);
+		console.log("SMAE " + this._status + " " + this._connectionDeferred.promise.isFulfilled());
 		if(this._status === 'opened')
 			return Q(this.self());
 		else if(this._connectionDeferred && !this._connectionDeferred.promise.isFulfilled())
