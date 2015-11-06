eaaa2d9d7c62dca258f3762daf3d58bb8e44a9dd
jfellus <jerome.fellus@partnering.fr>
2015-11-06 15:56:37 +0100

    fixed null selector matching bug

--- src/DiyaSelector.js
+++ src/DiyaSelector.js
@@ -90,6 +90,7 @@ inherits(DiyaSelector, EventEmitter);
 
 
 function match(selector, str){
+	if(!selector) return false;
 	if(selector === "#self") return connection && str===connection.self();
 	else if(selector.constructor.name === 'String'){
 		return matchString(selector, str);
