c39f5eb6536c8cc19404d6ee363fb45f9a724be4
Roberto Castellini <roberto.castellini@partnering.fr>
2017-03-02 16:09:53 +0100

    Delete useless comments

--- src/services/ieq/ieq.js
+++ src/services/ieq/ieq.js
@@ -394,11 +394,9 @@ IEQ.prototype.getCSVData = function(sensorNames,_firstDay,callback){
 
 
 IEQ.prototype.getHeatMapData = function(sensorNames,time, sample, callback){
-	//var startParse = new Date(startEpoch);
-	//var endParse = new Date(endEpoch);
 	var dataConfig = {
 		criteria: {
-			time: {start: time.startEpoch, end: time.endEpoch, sampling: sample}, // 360h -> 15d // 180h -> 7j
+			time: {start: time.startEpoch, end: time.endEpoch, sampling: sample}, 
 			places: [],
 			robots: []
 		},
