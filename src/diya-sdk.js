fe00797f90a7bed5ecf735a7539c258e520f3664
stephanieder <stephanieclaire.deroubaix@gmail.com>
2017-09-14 18:09:25 +0200

    remove commented requirements

--- src/diya-sdk.js
+++ src/diya-sdk.js
@@ -1,12 +1,8 @@
 var d1 = require('./DiyaSelector.js');
 
-// require('./services/rtc/rtc.js');
 require('./services/ieq/ieq.js');
-// require('./services/peerAuth/PeerAuth.js');
 require('./services/meshNetwork/MeshNetwork.js');
 require('./utils/encoding/encoding.js');
-// require('./services/status/status.js');
 require('./DBusObject.js');
-// require('./services/shell/Shell.js');
 
 module.exports = d1;
