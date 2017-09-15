e29252a4febb9d346a4278b41e69435827f5f8fd
stepdero <stephanie.deroubaix@partnering.fr>
2017-09-15 18:00:41 +0200

    remove requirement of ieq service

--- src/diya-sdk.js
+++ src/diya-sdk.js
@@ -1,6 +1,5 @@
 var d1 = require('./DiyaSelector.js');
 
-require('./services/ieq/ieq.js');
 require('./services/meshNetwork/MeshNetwork.js');
 require('./utils/encoding/encoding.js');
 require('./DBusObject.js');
